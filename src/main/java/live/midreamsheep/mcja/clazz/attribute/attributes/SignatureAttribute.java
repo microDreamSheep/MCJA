0f941d8b047a30a21b103c6b931a37ca830c5585
microDreamSheep <atthemc313128460@gmail.com>
2023-04-15 19:38:38 +0800

    属性实现

--- src/main/java/live/midreamsheep/mcja/clazz/attribute/attributes/SignatureAttribute.java
+++ src/main/java/live/midreamsheep/mcja/clazz/attribute/attributes/SignatureAttribute.java
@@ -1,4 +1,31 @@
 package live.midreamsheep.mcja.clazz.attribute.attributes;
 
-public class SignatureAttribute {
+import live.midreamsheep.mcja.clazz.attribute.AttributeEnum;
+import live.midreamsheep.mcja.clazz.attribute.MCJAAttributeAbstract;
+import live.midreamsheep.mcja.clazz.attribute.MCJAAttributeInterface;
+import live.midreamsheep.mcja.error.IllDataTypeException;
+import live.midreamsheep.mcja.tool.FormatConversion;
+
+public class SignatureAttribute extends MCJAAttributeAbstract {
+
+    private static final AttributeEnum type = AttributeEnum.SIGNATURE;
+
+    private short signatureIndex;
+    public void setSignatureIndex(String signature) {
+        signatureIndex = context.getConstant().getUtf8Index(signature);
+    }
+
+    @Override
+    public MCJAAttributeInterface loadData() throws IllDataTypeException {
+        setData(FormatConversion.asByteArray(new short[]{signatureIndex}));
+        return this;
+    }
+
+    @Override
+    public AttributeEnum getType() {
+        return type;
+    }
+    public SignatureAttribute(){
+        index = context.getConstant().getUtf8Index(type);
+    }
 }
--- src/main/java/live/midreamsheep/mcja/clazz/attribute/attributes/SourceDebugExtensionAttribute.java
+++ src/main/java/live/midreamsheep/mcja/clazz/attribute/attributes/SourceDebugExtensionAttribute.java
@@ -1,4 +1,31 @@
 package live.midreamsheep.mcja.clazz.attribute.attributes;
 
-public class SourceDebugExtensionAttribute {
+import live.midreamsheep.mcja.clazz.attribute.AttributeEnum;
+import live.midreamsheep.mcja.clazz.attribute.MCJAAttributeAbstract;
+import live.midreamsheep.mcja.clazz.attribute.MCJAAttributeInterface;
+import live.midreamsheep.mcja.error.IllDataTypeException;
+
+public class SourceDebugExtensionAttribute extends MCJAAttributeAbstract {
+
+    private static final AttributeEnum type = AttributeEnum.SOURCE_DEBUG_EXTENSION;
+
+    @Override
+    public void setData(byte[] data) throws IllDataTypeException {
+        super.setData(data);
+    }
+
+    public SourceDebugExtensionAttribute() {
+        index = context.getConstant().getUtf8Index(type);
+    }
+
+
+    @Override
+    public MCJAAttributeInterface loadData() throws IllDataTypeException {
+        throw new IllDataTypeException("SourceDebugExtensionAttribute can't load data");
+    }
+
+    @Override
+    public AttributeEnum getType() {
+        return type;
+    }
 }
